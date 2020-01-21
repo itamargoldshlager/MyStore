6f88daaeb9af3141bc0f45618ec334ebe9c83247
itamargold <itamargold7@gmail.com>
2020-01-21 21:04:11 +0200

    hi

---
+++ src/MyStore/Components/MainPage/Header.tsx
@@ -0,0 +1,71 @@
+import React, {FC} from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import clsx from "clsx";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {Link} from "react-router-dom";
+import "./linkStyle.css"
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: 36,
+        },
+        hide: {
+            display: 'none',
+        },
+    }),
+);
+
+const Header: FC<{open: boolean, handleDrawerOpen: () => void}> = ({open, handleDrawerOpen}) => {
+    const classes = useStyles();
+    return (
+        <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+                [classes.appBarShift]: open,
+            })}
+        >
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    className={clsx(classes.menuButton, {
+                        [classes.hide]: open,
+                    })}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Link to="/home" className="LinkStyle">
+                    <Typography variant="h6" noWrap>
+                        MyStore
+                    </Typography>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
---
+++ src/MyStore/Components/MainPage/SideBarDrawer.tsx
@@ -0,0 +1,74 @@
+import React, {FC} from 'react';
+import clsx from "clsx";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Divider from "@material-ui/core/Divider";
+import SideBarList from "./SideBarList";
+import Drawer from "@material-ui/core/Drawer";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+            whiteSpace: 'nowrap',
+        },
+        drawerOpen: {
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        drawerClose: {
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            overflowX: 'hidden',
+            width: theme.spacing(7) + 1,
+            [theme.breakpoints.up('sm')]: {
+                width: theme.spacing(9) + 1,
+            },
+        },
+        toolbar: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+        },
+    }),
+);
+
+const SideBarDrawer: FC<{open: boolean, handleDrawerClose: () => void}> = ({open, handleDrawerClose}) => {
+    const classes = useStyles();
+    return (
+        <Drawer
+            variant="permanent"
+            className={clsx(classes.drawer, {
+                [classes.drawerOpen]: open,
+                [classes.drawerClose]: !open,
+            })}
+            classes={{
+                paper: clsx({
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                }),
+            }}
+        >
+            <div className={classes.toolbar}>
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronRightIcon />
+                </IconButton>
+            </div>
+            <Divider />
+            <SideBarList/>
+        </Drawer>
+    );
+};
+
+export default SideBarDrawer;
